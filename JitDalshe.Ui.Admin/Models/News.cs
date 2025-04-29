da649eee07403e76036b59c4c9a39d3d5ca967ab
sug4chy <sb.9226981433@gmail.com>
2025-04-29 17:53:31 +0500

    Add PrimaryPhoto selection through radio buttons

--- JitDalshe.Ui.Admin/Models/News.cs
+++ JitDalshe.Ui.Admin/Models/News.cs
@@ -1,11 +1,12 @@
 namespace JitDalshe.Ui.Admin.Models;
 
-public readonly record struct News(
+public sealed record News(
     Guid Id,
-    string Text,
     DateOnly PublishDate,
     NewsPhoto[] Photos)
 {
+    public string Text { get; set; } = string.Empty;
+
     public string Title => Text.Split('.')[0];
 
     public string TextWithoutTitle => string.Join('.', Text.Split('.')[1..]);
