65f5bf0645db60e671832eb1d6a5f94657393734
sug4chy <sb.9226981433@gmail.com>
2025-05-06 19:30:49 +0500

    Пофиксил баг с выбором основного фото для новости

--- JitDalshe.Ui.Admin/Models/GuidReference.cs
+++ JitDalshe.Ui.Admin/Models/GuidReference.cs
@@ -1,8 +1,31 @@
+using System.ComponentModel;
+using System.Globalization;
+
 namespace JitDalshe.Ui.Admin.Models;
 
-public class GuidReference
+[TypeConverter(typeof(GuidReferenceTypeConverter))]
+public sealed class GuidReference
 {
     public Guid Value { get; set; }
 
     public static implicit operator Guid(GuidReference reference) => reference.Value;
+}
+
+public sealed class GuidReferenceTypeConverter : TypeConverter
+{
+    public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
+        => sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+
+    public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+    {
+        if (value is not string stringValue)
+        {
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        return new GuidReference
+        {
+            Value = Guid.Parse(stringValue)
+        };
+    }
 }
