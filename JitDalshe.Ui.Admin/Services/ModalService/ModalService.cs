1f75a8fb1bc883c382be20d17714f36fcd845cf4
sug4chy <sb.9226981433@gmail.com>
2025-06-23 16:42:59 +0500

    feat: add error handling to services

--- JitDalshe.Ui.Admin/Services/ModalService/ModalService.cs
+++ JitDalshe.Ui.Admin/Services/ModalService/ModalService.cs
@@ -1,3 +1,5 @@
+using Blazored.Toast.Services;
+using JitDalshe.Ui.Admin.Extensions;
 using Microsoft.JSInterop;
 
 namespace JitDalshe.Ui.Admin.Services.ModalService;
@@ -5,19 +7,36 @@ namespace JitDalshe.Ui.Admin.Services.ModalService;
 public sealed class ModalService : IModalService
 {
     private readonly IJSRuntime _jsRuntime;
+    private readonly IToastService _toastService;
 
-    public ModalService(IJSRuntime jsRuntime)
+    public ModalService(IJSRuntime jsRuntime, IToastService toastService)
     {
         _jsRuntime = jsRuntime;
+        _toastService = toastService;
     }
 
-    public async Task ShowModalAsync(string id, CancellationToken ct = default)
+    private Task RunShowingToastOnExceptionAsync(Func<Task> action)
     {
-        await _jsRuntime.InvokeVoidAsync(JsInteropConstants.ShowModal, ct, id);
+        try
+        {
+            return action();
+        }
+        catch (Exception e)
+        {
+            _toastService.ShowPermanentError(e);
+            return Task.CompletedTask;
+        }
     }
 
-    public async Task HideModalAsync(string id, CancellationToken ct = default)
-    {
-        await _jsRuntime.InvokeVoidAsync(JsInteropConstants.HideModal, ct, id);
-    }
+    public Task ShowModalAsync(string id, CancellationToken ct = default)
+        => RunShowingToastOnExceptionAsync(async () =>
+        {
+            await _jsRuntime.InvokeVoidAsync(JsInteropConstants.ShowModal, ct, id);
+        });
+
+    public Task HideModalAsync(string id, CancellationToken ct = default)
+        => RunShowingToastOnExceptionAsync(async () =>
+        {
+            await _jsRuntime.InvokeVoidAsync(JsInteropConstants.HideModal, ct, id);
+        });
 }
--- JitDalshe.Ui.Admin/Services/SwiperService/SwiperService.cs
+++ JitDalshe.Ui.Admin/Services/SwiperService/SwiperService.cs
@@ -1,3 +1,5 @@
+using Blazored.Toast.Services;
+using JitDalshe.Ui.Admin.Extensions;
 using Microsoft.JSInterop;
 
 namespace JitDalshe.Ui.Admin.Services.SwiperService;
@@ -5,14 +7,30 @@ namespace JitDalshe.Ui.Admin.Services.SwiperService;
 public sealed class SwiperService : ISwiperService
 {
     private readonly IJSRuntime _jsRuntime;
+    private readonly IToastService _toastService;
 
-    public SwiperService(IJSRuntime jsRuntime)
+    public SwiperService(IJSRuntime jsRuntime, IToastService toastService)
     {
         _jsRuntime = jsRuntime;
+        _toastService = toastService;
     }
 
-    public async Task InitSwiperAsync(CancellationToken ct = default)
+    private Task RunShowingToastOnExceptionAsync(Func<Task> action)
     {
-        await _jsRuntime.InvokeVoidAsync(JsInteropConstants.InitSwiper, ct);
+        try
+        {
+            return action();
+        }
+        catch (Exception e)
+        {
+            _toastService.ShowPermanentError(e);
+            return Task.CompletedTask;
+        }
     }
+
+    public Task InitSwiperAsync(CancellationToken ct = default)
+        => RunShowingToastOnExceptionAsync(async () =>
+        {
+            await _jsRuntime.InvokeVoidAsync(JsInteropConstants.InitSwiper, ct);
+        });
 }
