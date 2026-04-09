0f12db9639e520279bda1b389df09840083ad9fb
Soniccooles <andrey.sonic@mail.ru>
2026-04-09 13:26:45 +0500

    feat (query): Take and validate parameters from query from frontend fix (docker): build site api instead of taking image from dockerhub (temporary)

--- JitDalshe.Api.Site/Requests/ListWithPaginationRequest.cs
+++ JitDalshe.Api.Site/Requests/ListWithPaginationRequest.cs
@@ -1,17 +1,24 @@
 using FluentValidation;
 using JitDalshe.Api.Attributes;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JitDalshe.Api.Site.Requests;
 
 
 [Validator<ListWithPaginationRequestValidator>]
-public sealed record ListWithPaginationRequest(int PageNumber, int PageSize);
+public sealed record ListWithPaginationRequest(
+    [FromQuery(Name = "offset")] int Offset = 0, 
+    [FromQuery(Name = "limit")] int Limit = 10
+) {
+    public int PageNumber => Offset + 1;
+    public int PageSize => Limit;
+}
 
 public sealed class ListWithPaginationRequestValidator : AbstractValidator<ListWithPaginationRequest>
 {
     public ListWithPaginationRequestValidator()
     {
-        RuleFor(x => x.PageNumber).GreaterThan(0);
-        RuleFor(x => x.PageSize).GreaterThan(0);
+        RuleFor(x => x.Offset).GreaterThanOrEqualTo(0);
+        RuleFor(x => x.Limit).GreaterThan(0).LessThanOrEqualTo(100);
     }
 }
