63f8e313394e9c53a5002bd285e15f3f3192ebbb
Soniccooles <andrey.sonic@mail.ru>
2026-04-15 18:19:52 +0500

    Described FindAllAsync, FindByIdAsync and EditAsync functions in IConsultationRequestsRepository.cs

--- Application/JitDalshe.Application/Abstractions/Repositories/IConsultationRequestsRepository.cs
+++ Application/JitDalshe.Application/Abstractions/Repositories/IConsultationRequestsRepository.cs
@@ -1,8 +1,24 @@
+using System.Linq.Expressions;
+using CSharpFunctionalExtensions;
+using JitDalshe.Application.Enums;
 using JitDalshe.Domain.Entities.Consultations;
+using JitDalshe.Domain.ValueObjects;
 
 namespace JitDalshe.Application.Abstractions.Repositories;
 
 public interface IConsultationRequestsRepository
 {
+    Task<ConsultationRequest[]> FindAllAsync<TOrderKey>(
+        int? pageNumber = null,
+        int? pageSize = null,
+        Expression<Func<ConsultationRequest, bool>>? filteringExpression = null,
+        Expression<Func<ConsultationRequest, TOrderKey>>? orderByExpression = null,
+        SortingOrder sortingOrder = SortingOrder.Ascending,
+        CancellationToken ct = default);
+    
+    Task<Maybe<ConsultationRequest>> FindByIdAsync(IdOf<ConsultationRequest> id, CancellationToken ct = default);
+    
     Task AddAsync(ConsultationRequest request, CancellationToken ct = default);
+    
+    Task EditAsync(ConsultationRequest request, CancellationToken ct = default);
 }
